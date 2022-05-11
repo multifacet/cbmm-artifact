c37371bda0b7d970bb07ee22a1e68a550b4d0655
Mark Mansi <markm@cs.wisc.edu>
2022-05-11 14:15:12 -0500

    fix paths in fig 5, 6 scripts

--- scripts/figure-6-linux-cbmm.sh
+++ scripts/figure-6-linux-cbmm.sh
@@ -32,22 +32,22 @@ USER=$2
 
 # **CBMM, without fragmentation**
 
-./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic  --asynczero --mm_econ --mm_econ_benefit_file ./profiles/xz-training.csv hacky_spec17 xz --spec_size 76800 --input  testing
-./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic  --asynczero --mm_econ --mm_econ_benefit_file ./profiles/mcf.csv hacky_spec17 mcf
-./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic  --asynczero --mm_econ --mm_econ_benefit_file ./profiles/canneal.csv canneal --rand 530000000
-./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic  --asynczero --mm_econ --mm_econ_benefit_file ./profiles/mongodb.csv mongodb --op_count 9500000 --read_prop 0.25 --update_prop 0.375
-./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic  --asynczero --mm_econ --mm_econ_benefit_file ./profiles/memcached.csv memcachedycsb 150 --op_count 9400000 --read_prop 0.99 --update_prop 0.01
-./target/debug/runner exp00012 ${MACHINE} ${USER} --smaps_periodic --instrument redis-server  --asynczero --mm_econ --mm_econ_benefits redis-server ./profiles/mix-redis.csv mixycsb 150 --op_count 9400000 --read_prop 0.50 --update_prop 0.25
-./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic  --asynczero --mm_econ --mm_econ_benefit_file ./profiles/thp-ubmk.csv thp_ubmk 150
-./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic  --asynczero --mm_econ --mm_econ_benefit_file ./profiles/mix-redis.csv redisycsb 50 --op_count 9400000 --read_prop 0.50 --update_prop 0.25
+./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic  --asynczero --mm_econ --mm_econ_benefit_file ../../profiles/xz-training.csv hacky_spec17 xz --spec_size 76800 --input  testing
+./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic  --asynczero --mm_econ --mm_econ_benefit_file ../../profiles/mcf.csv hacky_spec17 mcf
+./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic  --asynczero --mm_econ --mm_econ_benefit_file ../../profiles/canneal.csv canneal --rand 530000000
+./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic  --asynczero --mm_econ --mm_econ_benefit_file ../../profiles/mongodb.csv mongodb --op_count 9500000 --read_prop 0.25 --update_prop 0.375
+./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic  --asynczero --mm_econ --mm_econ_benefit_file ../../profiles/memcached.csv memcachedycsb 150 --op_count 9400000 --read_prop 0.99 --update_prop 0.01
+./target/debug/runner exp00012 ${MACHINE} ${USER} --smaps_periodic --instrument redis-server  --asynczero --mm_econ --mm_econ_benefits redis-server ../../profiles/mix-redis.csv mixycsb 150 --op_count 9400000 --read_prop 0.50 --update_prop 0.25
+./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic  --asynczero --mm_econ --mm_econ_benefit_file ../../profiles/thp-ubmk.csv thp_ubmk 150
+./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic  --asynczero --mm_econ --mm_econ_benefit_file ../../profiles/mix-redis.csv redisycsb 50 --op_count 9400000 --read_prop 0.50 --update_prop 0.25
 
 # **CBMM, with fragmentation**
 
-./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic --fragmentation 100 --asynczero --mm_econ --mm_econ_benefit_file ./profiles/xz-training.csv hacky_spec17 xz --spec_size 76800 --input  testing
-./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic --fragmentation 100 --asynczero --mm_econ --mm_econ_benefit_file ./profiles/mcf.csv hacky_spec17 mcf
-./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic --fragmentation 100 --asynczero --mm_econ --mm_econ_benefit_file ./profiles/canneal.csv canneal --rand 530000000
-./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic --fragmentation 100 --asynczero --mm_econ --mm_econ_benefit_file ./profiles/mongodb.csv mongodb --op_count 9500000 --read_prop 0.25 --update_prop 0.375
-./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic --fragmentation 100 --asynczero --mm_econ --mm_econ_benefit_file ./profiles/memcached.csv memcachedycsb 150 --op_count 9400000 --read_prop 0.99 --update_prop 0.01
-./target/debug/runner exp00012 ${MACHINE} ${USER} --smaps_periodic --instrument redis-server --fragmentation 100 --asynczero --mm_econ --mm_econ_benefits redis-server ./profiles/mix-redis.csv mixycsb 150 --op_count 9400000 --read_prop 0.50 --update_prop 0.25
-./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic --fragmentation 100 --asynczero --mm_econ --mm_econ_benefit_file ./profiles/thp-ubmk.csv thp_ubmk 150
-./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic --fragmentation 100 --asynczero --mm_econ --mm_econ_benefit_file ./profiles/mix-redis.csv redisycsb 50 --op_count 9400000 --read_prop 0.50 --update_prop 0.25
+./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic --fragmentation 100 --asynczero --mm_econ --mm_econ_benefit_file ../../profiles/xz-training.csv hacky_spec17 xz --spec_size 76800 --input  testing
+./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic --fragmentation 100 --asynczero --mm_econ --mm_econ_benefit_file ../../profiles/mcf.csv hacky_spec17 mcf
+./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic --fragmentation 100 --asynczero --mm_econ --mm_econ_benefit_file ../../profiles/canneal.csv canneal --rand 530000000
+./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic --fragmentation 100 --asynczero --mm_econ --mm_econ_benefit_file ../../profiles/mongodb.csv mongodb --op_count 9500000 --read_prop 0.25 --update_prop 0.375
+./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic --fragmentation 100 --asynczero --mm_econ --mm_econ_benefit_file ../../profiles/memcached.csv memcachedycsb 150 --op_count 9400000 --read_prop 0.99 --update_prop 0.01
+./target/debug/runner exp00012 ${MACHINE} ${USER} --smaps_periodic --instrument redis-server --fragmentation 100 --asynczero --mm_econ --mm_econ_benefits redis-server ../../profiles/mix-redis.csv mixycsb 150 --op_count 9400000 --read_prop 0.50 --update_prop 0.25
+./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic --fragmentation 100 --asynczero --mm_econ --mm_econ_benefit_file ../../profiles/thp-ubmk.csv thp_ubmk 150
+./target/debug/runner exp00010 ${MACHINE} ${USER} --smaps_periodic --fragmentation 100 --asynczero --mm_econ --mm_econ_benefit_file ../../profiles/mix-redis.csv redisycsb 50 --op_count 9400000 --read_prop 0.50 --update_prop 0.25
